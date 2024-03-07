5bbcaefecab861db4938e05c97f84a2b0cfece47
Lucas Máximo <lucasmaximo@outlook.com>
2024-03-06 22:40:51 -0300

    Adicionar arquivos de projeto.

---
+++ FixacaoExcecoes/Entities/Conta.cs
@@ -0,0 +1,45 @@
+﻿using FixacaoExcecoes.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixacaoExcecoes.Entities
+{
+    internal class Conta
+    {
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+        public double LimiteSaque { get; set; }
+
+        public Conta() { }
+
+        public Conta(int numero, string titular, double saldo, double limiteSaque)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+            LimiteSaque = limiteSaque;
+        }
+
+        public void Depositar(double quantia)
+        {
+            Saldo += quantia;
+        }
+        public void Sacar(double quantia)
+        {
+            if (quantia > LimiteSaque)
+            {
+                throw new DomainException("Esse valor excede o limite de saque");
+            }
+            if (quantia > Saldo)
+            {
+                throw new DomainException("Saldo insuficiente para realizar o saque");
+            }
+
+            Saldo -= quantia;
+        }
+    }
+}
---
+++ FixacaoExcecoes/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FixacaoExcecoes.Entities;
+using FixacaoExcecoes.Entities.Exceptions;
+using System.Globalization;
+
+Console.WriteLine("Entre os dados da conta");
+
+Console.Write("Número: ");
+int numero = int.Parse(Console.ReadLine());
+Console.Write("Titular: ");
+string titular = (Console.ReadLine());
+Console.Write("Saldo inicial: ");
+double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Limite de saque: ");
+double limiteSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Conta conta = new Conta(numero, titular, saldo, limiteSaque);
+
+try
+{
+    Console.WriteLine();
+    Console.Write("Entre o valor para sacar: ");
+    double quantia = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+    conta.Sacar(quantia);
+    Console.WriteLine("Novo saldo: " + conta.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+}
+catch (DomainException e)
+{
+    Console.WriteLine("Erro no saque: " + e.Message);
+}
